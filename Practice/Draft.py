f96b303c581553a544abaa17f59c9b5d655859bd
Ivandolchevic <idolchevic@gmail.com>
2018-01-14 07:59:45 -0800

    filtering with .gitignore

--- Practice/Draft.py
+++ Practice/Draft.py
@@ -1,1 +1,71 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+
+# Import the necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+def fnZero():
+    df = pd.read_csv("population.csv")
+
+    y = df['child']
+    del df['child']
+    X = df
+    return X,y
+
+def fnOne():
+    X,y = fnZero()
+
+    # Create training and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+    # Instantiate a k-NN classifier: knn
+    knn = KNeighborsClassifier(n_neighbors=6)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+
+    # Predict the labels of the test data: y_pred
+    y_pred = knn.predict(X_test)
+
+    # Generate the confusion matrix and classification report
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+
+def fnTwo():
+    X,y = fnZero()
+
+    # Create training and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+    # Create the classifier: logreg
+    logreg = LogisticRegression()
+
+    # Fit the classifier to the training data
+    logreg.fit(X_train, y_train)
+
+    # Import necessary modules
+    from sklearn.metrics import roc_curve
+
+    # Compute predicted probabilities: y_pred_prob
+    y_pred_prob = logreg.predict_proba(X_test)[:, 1]
+
+    # Generate ROC curve values: fpr, tpr, thresholds
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+
+    # Plot ROC curve
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.show()
+
+fnTwo()
+
