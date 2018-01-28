18cb1b08c3d21b302f96f745f63b446fda676bfb
Ivandolchevic <idolchevic@gmail.com>
2018-01-28 18:04:02 +0100

    Supervised Learning - 29%

--- SupervisedLearningWithScikitLearn/PreprocessingAndPipelines.py
+++ SupervisedLearningWithScikitLearn/PreprocessingAndPipelines.py
@@ -1,85 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+def fnInit():
+    df = pd.read_csv('gm_2008_region.csv')
 
+    # Create dummy variables with drop_first=True: df_region
+    df_region = pd.get_dummies(df, drop_first=True)
+    y = df_region['life']
+    del df_region['life']
+    X = df_region
+    return X,y
 
+def fnExploring():
 
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+    # Read 'gapminder.csv' into a DataFrame: df
+    df = pd.read_csv('gm_2008_region.csv')
 
+    # Create a boxplot of life expectancy per region
+    df.boxplot('life', 'Region', rot=60)
 
+    # Show the plot
+    plt.show()
 
+def fnCreateDummies():
+    # Read 'gapminder.csv' into a DataFrame: df
+    df = pd.read_csv('gm_2008_region.csv')
 
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+    # Create dummy variables: df_region
+    df_region = pd.get_dummies(df)
 
+    # Print the columns of df_region
+    print(df_region.columns)
 
+    # Create dummy variables with drop_first=True: df_region
+    df_region = pd.get_dummies(df, drop_first=True)
 
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+    # Print the new columns of df_region
+    print(df_region.columns)
 
+def fnRidgeRegression():
+    X,y= fnInit()
 
+    # Import necessary modules
+    from sklearn.linear_model import Ridge
+    from sklearn.model_selection import cross_val_score
 
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+    # Instantiate a ridge regressor: ridge
+    ridge = Ridge(alpha=0.5, normalize=True)
 
+    # Perform 5-fold cross-validation: ridge_cv
+    ridge_cv = cross_val_score(ridge, X, y, cv=5)
 
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
+    # Print the cross-validated scores
+    print(ridge_cv)
 
 
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
-
-print("---------------------------------------------------")
-print("   ")
-print("---------------------------------------------------")
-
-
+fnRidgeRegression()
